854b8ea98d68c9f38beca8b29dd6d3a604d5d580
Hari Dhole <haridhole15196@gmail.com>
2026-05-30 17:08:35 +0530

    setup prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,22 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect( ()=>{
-    fetch("/api/hello").then((res)=> res.json()).then(data=> setMessage(data.message));
-  },[]);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <p className="text-3xl font-bold">{message}</p>
-      <Button>Click Me</Button>
-    </div>
-  )
+   return (
+      <div className="p-4">
+         <p className="text-3xl font-bold">{message}</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
